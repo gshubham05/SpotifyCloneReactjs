cea3b2b5f5dc206b2ceb7a0eb97d395c70ffaf67
gshubham05 <gshubham.05@gmail.com>
2024-12-12 14:07:16 +0530

    Ui is ready

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Display from './components/Display'
+import Player from './components/Player'
+import Sidebar from './components/Sidebar'
+
+function App() {
+
+
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
+    </div>
+  )
+}
+
+export default App
